ee0681759c6ca95ca9e7b95df9f6b4311a42a426
romain <romain.legal44@gmail.com>
2019-09-12 12:48:16 +0200

    create seach type and filters

--- src/Controller/PropertiesController.php
+++ src/Controller/PropertiesController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
@@ -35,14 +37,19 @@ class PropertiesController extends  AbstractController
      */
     public function index(PaginatorInterface $paginator, Request $request) : Response
     {
-            $properties = $paginator->paginate(
-            $this->repository->findAllVisibleQuery(),
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery($search),
             $request->query->getInt('page',1),
             12
-            );
+        );
         return $this->render("property/index.html.twig",[
             'current_menu' => 'properties',
-            'properties' => $properties
+            'properties' => $properties,
+            'form' => $form->createView()
 
         ]);
 
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\ORM\Query;
@@ -25,10 +26,23 @@ class PropertyRepository extends ServiceEntityRepository
     /**
      * @return Query
      */
-    public function  findAllVisibleQuery() : Query
+    public function  findAllVisibleQuery(PropertySearch $search) : Query
     {
-        return $this->findVisibleQuery()
-        ->getQuery();
+        $query = $this->findVisibleQuery();
+
+        if ($search->getMaxPrice()) {
+            $query = $query
+                ->andWhere('p.price <= :maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+        }
+
+        if ($search->getMinSurface()) {
+            $query = $query
+                ->andWhere('p.surface >= :minSurface')
+                ->setParameter('minSurface', $search->getMinSurface());
+        }
+
+        return $query->getQuery();
 
     }
 
